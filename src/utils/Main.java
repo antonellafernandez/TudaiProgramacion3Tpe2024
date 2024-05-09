0d22e9e3c1aa9f605fa075ad1d1b3e1220fc4bfb
Daniela Fernández <liquiddookie.17@gmail.com>
2024-05-09 20:58:55 -0300

    Añadido esqueleto.

---
+++ src/utils/Main.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class Main {
+    public static void main(String[] args) {
+        Servicios servicios = new Servicios("./src/datasets/Procesadores.csv", "./src/datasets/Tareas.csv");
+    }
+}
